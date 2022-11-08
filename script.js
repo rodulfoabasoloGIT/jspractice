7ad83b71ec599a44a1687e24f4a209ffeec1fa23
Abasol <44283590+rodulfoabasoloGIT@users.noreply.github.com>
2022-11-08 12:07:20 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function myFunction() {
+//   console.log("gwapo ko kining ina nyo");
+// }
+
+function myFunction() {
+  const valueTest = document.getElementById("todo").value;
+  const todoApp = document.getElementById("value");
+}
